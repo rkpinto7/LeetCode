58ef99f6d861a62ef6f65b0a75e96aca10d9128b
rkpinto7 <120949051+rkpinto7@users.noreply.github.com>
2023-07-18 13:42:48 +0530

    Time: 185 ms (9.29%) | Memory: 16.6 MB (25.44%) - LeetSync

---
+++ 39-combination-sum/combination-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        hash_set = set()
+        return_list = []
+
+        def backtracker(curr_arr, curr_sum):
+            if curr_sum == target:
+                curr_arr.sort()
+                if tuple(curr_arr) not in hash_set:
+                    hash_set.add(tuple(curr_arr))
+                    return_list.append(curr_arr[:])  # Use 
+            else:
+                for num in candidates:
+                    if curr_sum + num <= target:
+                        curr_arr.append(num)
+                        backtracker(curr_arr[:], curr_sum + num)
+                        curr_arr.pop()
+                    else:
+                        break
+
+        backtracker([], 0)
+        return return_list
